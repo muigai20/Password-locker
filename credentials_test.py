e2802782a1aac2d6d9bb265befb23ba069eece3c
SamuelOkoth <okothsamuel78@gmail.com>
2019-10-06 17:07:38 +0300

    created tearDown  and setUp method to execute instructions

--- credentials_test.py
+++ credentials_test.py
@@ -5,4 +5,13 @@ class TestCredentials(unittest.TestCase):
     '''
     class to test credential class bahavior
     '''
-    
+def setUp(self):
+    '''
+    setUp methods to define instructions
+    '''
+    self.new_credentials = Credentials("Jose","Ian","Peter")
+def tearDown(self):
+    '''
+    Method that cleans up after each test
+    '''
+    Credentials.credentials_list = []
