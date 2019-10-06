ace6840da9580403d9c4ad87b65f9056537807f5
SamuelOkoth <okothsamuel78@gmail.com>
2019-10-06 16:36:43 +0300

    created a setUp method

--- user_test.py
+++ user_test.py
@@ -1,4 +1,13 @@
 import unittest
 from password import User
 
-class TestUser(unittest.TestCase)
+class TestUser(unittest.TestCase):
+    '''
+    class to test the behaviour of the test class
+    '''
+
+    def setUp(self):
+        '''
+        setUp method defines instruction
+        '''
+        self.new_user = User("Joseph","Kamau","Muigai")
