d2e5ed92bbf427689e563c722d823330806161e8
SamuelOkoth <okothsamuel78@gmail.com>
2019-10-06 16:42:55 +0300

    created a test to correct initialization

--- user_test.py
+++ user_test.py
@@ -10,4 +10,12 @@ class TestUser(unittest.TestCase):
         '''
         setUp method defines instruction
         '''
-        self.new_user = User("Joseph","Kamau","Muigai")
+        self.new_user = User("Joseph","Kamau","cooperative")
+    
+    def test_init(self):
+        '''
+        test to correct initialization
+        '''
+        self.assertEqual(self.new_user.first_name,"Joseph")
+        self.assertEqual(self.new_user.last_name,"Kamau")
+        self.assertEqual(self.new_user.password,"cooperative")
