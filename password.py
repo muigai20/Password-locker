bcca3f4f14273968aeaf6e400e6a79cfa26296c1
SamuelOkoth <okothsamuel78@gmail.com>
2019-10-06 10:47:45 +0300

    created a function to initialize user field

--- password.py
+++ password.py
@@ -6,3 +6,11 @@ class User:
     class that defines user login details in the application
     '''
     user_list = []
+
+    def __init__(self,first_name,last_name,password):
+        '''
+        fuction to initalize user field correctly
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
