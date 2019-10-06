f3139b60f33266c3102fa4d319f5e1153d9dbe8b
SamuelOkoth <okothsamuel78@gmail.com>
2019-10-06 17:22:37 +0300

    created a test to check whether credentials are saved by the app

--- credentials_test.py
+++ credentials_test.py
@@ -15,3 +15,21 @@ def tearDown(self):
     Method that cleans up after each test
     '''
     Credentials.credentials_list = []
+def test_init(self):
+    '''
+    test to correct initialization
+    '''
+    self.assertEqual(new_credentials.account_name,"Jose")
+    self.assertEqual(new_credentials.username,"Ian")
+    self.assertEqual(new_credentials.password,"Peter")
+def test_save_credentials(self):
+    """
+    Test to check whether app saves account credentials
+    """
+    self.new_credentials.save_credentials()
+    self.assertEqual(len(Credentials.credentials_list),1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
